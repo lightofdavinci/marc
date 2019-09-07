9ff8e6266c96f8cd0880054721458709d5af3609
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2019-09-07 02:12:10 +0200

    :sparkles: Install script

---
+++ install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+
+# This script is meant for quick & easy install via:
+# $ curl -sSL https://github.com/wouterds/rpi-quake3-server/raw/master/install.sh | sh
+
+echo 'Creating ./quake3 directory...'
+mkdir quake3
+cd quake3
+
+echo 'Downloading assets...'
+curl -LO https://github.com/wouterds/rpi-quake3-server/raw/master/docker-compose.yml
+curl -LO https://github.com/wouterds/rpi-quake3-server/raw/master/server.cfg
+curl -LO https://github.com/wouterds/rpi-quake3-server/releases/download/1.0.0/pak0.pk3
+
+echo ''
+echo 'Ready to go!'
+echo ''
+echo 'You can customize your server settings by editing: ./quake3/server.cfg'
+echo 'When ready, start the server by running "cd ./quake && docker-compose up -d"'
